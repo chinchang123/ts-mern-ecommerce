3b7e122c49f398697a19bcfb3311fce85ac577f0
chinchang <co22jitsopa@gmail.com>
2024-05-29 11:20:05 +0700

    Connect-To-MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,40 @@
-import express, { Request, Response } from 'express'
-import { sampleProducts } from './data'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+import { seedRouter } from './routers/seedRouter'
+import { productRouter } from './routers/productRouter'
+
+dotenv.config()
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb+srv://jitsopan:1234@cluster0.wsbtbe4.mongodb.net/tsmernbbvc?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
+
+
 const app = express()
 app.use(
   cors({
-    credentials:true,
+    credentials: true,
     origin: ['http://localhost:5173'],
   })
 )
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-  res.json(sampleProducts.find((x) => x.slug === req.params.slug))
-})
-app.get('/api/products', (req: Request, res: Response) => {
-  res.json(sampleProducts)
-})
+
+
+
+
+app.use('/api/products', productRouter)
+app.use('/api/seed', seedRouter)
+
+
 const PORT = 4000
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`)
