13fa80bec3b966f15e633c7f6dae70284f3acd58
chinchang <co22jitsopa@gmail.com>
2024-05-29 13:43:17 +0700

    Add To Cart

--- frontend/src/types/Product.ts
+++ frontend/src/types/Product.ts
@@ -1,13 +1,13 @@
 export type Product = {
-    name: string
-    slug: string
-    image: string
-    category: string
-    brand: string
-    price: number
-    countInStock: number
-    description: string
-    rating: number
-    numReviews: number
-  }
-  
+  name: string
+  _id: string
+  slug: string // แทนID
+  image: string
+  category: string
+  brand: string
+  price: number
+  countInStock: number
+  description: string
+  rating: number
+  numReviews: number
+}
